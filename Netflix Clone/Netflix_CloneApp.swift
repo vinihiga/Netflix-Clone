fd05414030fc3a2df216ac40653c3c0168a9f060
Vinicius Higa <viniciushigadev@gmail.com>
2025-03-22 21:19:00 -0300

    Initial Commit

---
+++ Netflix Clone/Netflix_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneApp.swift
+//  Netflix Clone
+//
+//  Created by Vinicius Higa on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
